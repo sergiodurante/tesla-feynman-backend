bf4300d94139ae7a83833c49b4c2dbc975840d91
sergiodurante <sergiodurante@me.com>
2025-06-08 20:20:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import openai
 import os
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/query', methods=['POST'])
 def query():
